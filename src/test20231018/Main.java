dee5272ebdfa4e31a6164a035f00dce6516ea43d
WNY <na010305@gmail.com>
2024-04-20 13:34:45 +0900

    two

--- src/test20231018/Main.java
+++ src/test20231018/Main.java
@@ -2,6 +2,7 @@ package test20231018;
 
 public class Main {
 	public static void main(String[] args) {
+		Two.print();
 		System.out.println("one 202114105");
 	}
 }
---
+++ src/test20231018/Two.java
@@ -0,0 +1,8 @@
+package test20231018;
+
+public class Two {
+    
+    public static void print() {
+        System.out.println("two");
+    }
+}
